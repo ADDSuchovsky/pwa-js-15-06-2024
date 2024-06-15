e0203ccd3a506ca9450e088d3923c6898767e8c1
addsuchovsky <addsuchovsky@gmail.com>
2024-06-15 10:51:43 +0200

    pwa

---
+++ sw.js
@@ -0,0 +1,27 @@
+let cacheName = "podyplomowe"
+let filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"]
+
+self.addEventListener('install', function(event) {
+    console.log('installing sw');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            let x = cache.addAll(urlsToCache);
+            console.log('cache added');
+            return x;
+        })
+    );
+});
+
+    self.addEventListener('fetch', function(event) {
+        event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+                return response;
+                }
+            return fetch(event.request);
+        })
+    );
+});
